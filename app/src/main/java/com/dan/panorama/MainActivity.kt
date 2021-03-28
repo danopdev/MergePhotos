9b862dfd8967adfbfcae380d0778d517cbba353f
dan <doprea@gmail.com>
2021-03-28 10:25:25 +0200

    Prepare app

--- app/src/main/java/com/dan/panorama/MainActivity.kt
+++ app/src/main/java/com/dan/panorama/MainActivity.kt
@@ -1,11 +1,75 @@
 package com.dan.panorama
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.dan.panorama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        val PERMISSIONS = arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+
+        const val REQUEST_PERMISSIONS = 1
+        const val INTENT_OPEN_IMAGES = 2
+
+        const val IMG_WORK_SIZE = 256
+    }
+
+    private val mBinding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        if (!askPermissions())
+            onPermissionsAllowed()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_PERMISSIONS -> handleRequestPermissions(grantResults)
+        }
+    }
+
+    private fun exitApp() {
+        setResult(0)
+        finish()
+    }
+
+    private fun askPermissions(): Boolean {
+        for (permission in PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, PERMISSIONS, REQUEST_PERMISSIONS)
+                return true
+            }
+        }
+
+        return false
+    }
+
+    private fun handleRequestPermissions(grantResults: IntArray) {
+        var allowedAll = grantResults.size >= PERMISSIONS.size
+
+        if (grantResults.size >= PERMISSIONS.size) {
+            for (result in grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    allowedAll = false
+                    break
+                }
+            }
+        }
+
+        if (allowedAll) onPermissionsAllowed()
+        else exitApp()
+    }
+
+    private fun onPermissionsAllowed() {
+        setContentView(mBinding.root)
     }
 }
