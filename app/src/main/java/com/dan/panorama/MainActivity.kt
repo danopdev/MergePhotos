ffbd893ef74015d734550177299b70348d6e96a3
Dan Oprea <dan.oprea@masternaut.com>
2021-03-31 17:15:56 +0200

    Add coroutines for async stitch

--- app/src/main/java/com/dan/panorama/MainActivity.kt
+++ app/src/main/java/com/dan/panorama/MainActivity.kt
@@ -10,16 +10,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.dan.panorama.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import org.bytedeco.opencv.global.opencv_core.CV_8UC3
 import org.bytedeco.opencv.global.opencv_imgcodecs.*
-import org.bytedeco.opencv.global.opencv_imgproc.WARP_FILL_OUTLIERS
 import org.bytedeco.opencv.global.opencv_imgproc.resize
 import org.bytedeco.opencv.opencv_core.Mat
 import org.bytedeco.opencv.opencv_core.MatVector
 import org.bytedeco.opencv.opencv_core.Size
 import org.bytedeco.opencv.opencv_stitching.CylindricalWarper
+import org.bytedeco.opencv.opencv_stitching.SphericalWarper
+import org.bytedeco.opencv.opencv_stitching.PlaneWarper
 import org.bytedeco.opencv.opencv_stitching.Stitcher
-import org.bytedeco.opencv.opencv_stitching.WarperCreator
-import org.opencv.core.CvType
 
 
 class MainActivity : AppCompatActivity() {
@@ -32,10 +35,14 @@ class MainActivity : AppCompatActivity() {
         const val REQUEST_PERMISSIONS = 1
         const val INTENT_OPEN_IMAGES = 2
 
-        const val IMG_WORK_SIZE = 256
+        const val IMG_WORK_SIZE = 512
+
+        const val PANORAMA_MODE_PLANE = 0
+        const val PANORAMA_MODE_CYLINDRICAL = 1
+        const val PANORAMA_MODE_SPHERICAL = 2
 
         fun matToBitmap(mat: Mat): Bitmap? {
-            if (CvType.CV_8UC3 != mat.type()) return null
+            if (CV_8UC3 != mat.type()) return null
 
             val width = mat.cols()
             val height = mat.rows()
@@ -48,7 +55,7 @@ class MainActivity : AppCompatActivity() {
             for (line in 0 until height) {
                 srcIndex = matArrayLineLength * line
                 for (column in 0 until width) {
-                    pixels[pixelIndex] = Color.rgb(
+                    pixels[pixelIndex] = Color.rgb( //Mat is in BGR format
                             matArrayData.getInt(srcIndex+2),
                             matArrayData.getInt(srcIndex+1),
                             matArrayData.getInt(srcIndex) )
@@ -63,7 +70,7 @@ class MainActivity : AppCompatActivity() {
         fun bitmapToMat(bitmap: Bitmap): Mat {
             val width = bitmap.width
             val height = bitmap.height
-            val mat = Mat(height, width, CvType.CV_8UC3)
+            val mat = Mat(height, width, CV_8UC3)
             var matIndex: Long
             val matArrayData = mat.arrayData()
             val matArrayLineLength = mat.arrayStep()
@@ -138,44 +145,77 @@ class MainActivity : AppCompatActivity() {
         else exitApp()
     }
 
+    private fun loadImage(path: String, small: Boolean): Mat {
+        val img = imread(path)
+        if (img.empty()) return img
+
+        Log.i("STITCHER", "Load OK: $path")
+        if (!small) return img
+
+        val widthSmall = IMG_WORK_SIZE
+        val heightSmall = IMG_WORK_SIZE * img.rows() / img.cols()
+        val imgSmall = Mat()
+        resize(img, imgSmall, Size(widthSmall, heightSmall))
+        return imgSmall
+    }
+
+    private fun loadImages( small: Boolean, l: (images: MatVector)->Unit) {
+        BusyDialog.show(supportFragmentManager)
+
+        GlobalScope.launch(Dispatchers.Default) {
+            val images = MatVector()
+            for (i in 1..5) {
+                val img = loadImage("/storage/emulated/0/Panorama/$i.jpg", small)
+                if (!img.empty()) {
+                    images.push_back(img)
+                }
+            }
+
+            runOnUiThread {
+                BusyDialog.dismiss()
+                l.invoke(images)
+            }
+        }
+    }
+
+    private fun makePanorama( images: MatVector, mode: Int, l: (panorama: Bitmap?)->Unit ) {
+        BusyDialog.show(supportFragmentManager)
+
+        GlobalScope.launch(Dispatchers.Default) {
+            Log.i("STITCHER", "Start")
+            val panoramaMat = Mat()
+            val stitcher = Stitcher.create(Stitcher.PANORAMA)
+
+            when(mode) {
+                PANORAMA_MODE_CYLINDRICAL -> stitcher.setWarper(CylindricalWarper())
+                PANORAMA_MODE_SPHERICAL -> stitcher.setWarper(SphericalWarper())
+                else -> stitcher.setWarper(PlaneWarper())
+            }
+
+            val status = stitcher.stitch(images, panoramaMat)
+            var panorama: Bitmap? = null
+
+            if (status == Stitcher.OK) {
+                Log.i("STITCHER", "Success")
+                panorama = matToBitmap(panoramaMat)
+            } else {
+                Log.i("STITCHER", "Failed")
+            }
+
+            runOnUiThread {
+                BusyDialog.dismiss()
+                l.invoke(panorama)
+            }
+        }
+    }
+
     private fun onPermissionsAllowed() {
         setContentView(mBinding.root)
 
-        /*
-        val images = MatVector()
-
-        for (i in 1..5) {
-            val img = imread("/storage/emulated/0/Panorama/$i.jpg")
-            if (img.empty()) {
-                Log.i("STITCHER", "Load FAILED: $i")
-                continue
+        loadImages(true) { images ->
+            makePanorama(images, PANORAMA_MODE_PLANE) { panorama ->
+                mBinding.imageView.setImageBitmap(panorama)
             }
-
-            Log.i("STITCHER", "Load OK: $i")
-            val widthSmall = IMG_WORK_SIZE
-            val heightSmall = IMG_WORK_SIZE * img.rows() / img.cols()
-
-            val imgSmall = Mat()
-            resize(img, imgSmall, Size(widthSmall, heightSmall))
-            Log.i("STITCHER", "Resized OK: $i")
-            images.push_back(imgSmall)
         }
-
-        Log.i("STITCHER", "Start")
-        val panorama = Mat()
-        val stitcher = Stitcher.create(Stitcher.PANORAMA)
-        stitcher.setWarper(CylindricalWarper())
-        stitcher.setInterpolationFlags(WARP_FILL_OUTLIERS)
-        val status = stitcher.stitch(images, panorama)
-
-        if (status == Stitcher.OK) {
-            Log.i("STITCHER", "Success")
-            matToBitmap(panorama)?.let { bitmap ->
-                mBinding.imageView.setImageBitmap(bitmap)
-            }
-        } else {
-            Log.i("STITCHER", "Failed")
-        }
-         */
     }
 }
